3fa11eaa779709e33f3c5d34ca6f4e0f6b6b2fae
RaedGhodhbane <ghodhbane.raed@gmail.com>
2021-11-06 14:58:50 +0100

    Create Start Game Screen Init

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './Components/Header';
+import StartGame from './Screens/StartGame';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header/>
+      <StartGame/>
     </View>
   );
 }
---
+++ Screens/StartGame.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet,View,Text } from 'react-native'
+
+export default function StartGame() {
+    return (
+        <View>
+            <Text> Start Game </Text>
+        </View>
+    )
+}
+
+const css=StyleSheet.create({
+
+})
