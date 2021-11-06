3aa48ab886540697e7cdf2d6a80f109250c7e600
RaedGhodhbane <ghodhbane.raed@gmail.com>
2021-11-06 15:02:07 +0100

    Create Input Text

---
+++ Components/InputText.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { StyleSheet, TextInput, View } from 'react-native'
+import colors from '../Global/colors'
+
+export default function InputText(props) {
+    return (
+            <TextInput style={css.text} {...props}
+            
+            />
+    )
+}
+
+const css = StyleSheet.create({
+    text : {
+        marginVertical : 20,
+        borderBottomColor : colors.greyColor,
+        borderBottomWidth : 2,
+        textAlign : "center",
+        height : 30,
+        width : 40
+
+
+    }
+})
--- Screens/StartGame.jsx
+++ Screens/StartGame.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { StyleSheet,View,Text } from 'react-native'
+import InputText from '../Components/InputText'
 
 export default function StartGame() {
     return (
-        <View>
-            <Text> Start Game </Text>
+        <View style={css.screen}>
+            <Text> Start Game Screen </Text>
+            <InputText/>
         </View>
     )
 }
 
 const css=StyleSheet.create({
-
+    screen : {
+        alignItems : "center",
+    }
 })
