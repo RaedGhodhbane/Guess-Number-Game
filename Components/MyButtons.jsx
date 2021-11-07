99bf79331e8d0d5dda28233b81c814da92ede398
RaedGhodhbane <ghodhbane.raed@gmail.com>
2021-11-07 20:48:54 +0100

    Create Buttons

---
+++ Components/MyButtons.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button, StyleSheet, View } from 'react-native'
+
+export default function MyButtons(props) {
+    return (
+        <Button {...props}
+        />
+    )
+}
+
+const css = StyleSheet.create ({
+    Button : {
+
+    }
+})
