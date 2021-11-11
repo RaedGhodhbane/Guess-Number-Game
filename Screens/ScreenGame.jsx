289a1fd7a696e8a5788dc3b0e5e31aa43ab38068
RaedGhodhbane <ghodhbane.raed@gmail.com>
2021-11-11 21:07:42 +0100

    Create ScreenGame & func handleChangeUserNumber

---
+++ Screens/ScreenGame.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function ScreenGame() {
+    return (
+        <View>
+            <Text> Screen Game </Text>
+        </View>
+    )
+}
+
+const css= StyleSheet.create({
+    
+})
